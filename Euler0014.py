606ba2753708705eaa699af06e25fd8d1a9b1cea
AndrewPlanchak <andy.planchak@gmail.com>
2023-10-15 21:51:53 +0100

    Finished Euler 0014

---
+++ Euler0014.py
@@ -0,0 +1,16 @@
+largest = 0
+
+for i in range(1, 1000000):
+    count = 0
+    n = i
+
+    while not n == 1:
+        if(n % 2 == 0): n = n / 2
+        else: n = 3 * n + 1
+        count += 1
+    
+    largest = max(count, largest)
+
+print(largest)
+
+# Finished - SLOW
